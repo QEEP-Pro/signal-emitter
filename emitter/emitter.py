51f582a632fdd085388c162458dfbeb01e5e8635
basilio <catred@mail2000.ru>
2017-11-05 06:56:23 +0700

    [emitter]

--- emitter/emitter.py
+++ emitter/emitter.py
@@ -2,20 +2,47 @@ import asyncio
 import websockets
 from random import random
 
-async def echo(websocket, path):
-    async for message in websocket:
-        await websocket.send("[{0},{1}]".format(random(),random()))
+'''
+    db: {
+        host     : 'localhost',
+        user     : 'admin',
+        password : 'admin',
+        database : 'digital-hack'
+    },
+    port: 3002,
+'''
 
-'''
-    def parse(self, data):
-        if "refreshData" in str(data):
+connection = dict()
+_id=0
+
+async def handler(websocket, path):
+    global _id
+    _id+=1
+    global connection
+    connection[websocket]=_id
+    print(path)
+    try:
+        if path=="/api/refreshData":
             pass
-        elif "parametersList" in str(data):
-            pass
-'''
-        
+        elif path=="/api/parametersList":
+            await websocket.send("[\"name\":,\"laws\":]")
+            lawsIdList=await websocket.recv()
+            print(lawsIdList)
+        else:
+            while len(connection)>0:
+                x,y=random(),random()
+                await asyncio.wait([ws.send("[\"id\":{},\"x\":{},\"y\":{}]".format(connection[ws],x,y)) for ws in connection])
+                await asyncio.sleep(random())
+    finally:
+        connection.pop(websocket,None)
+
+
 if __name__ == "__main__":
-    server=websockets.serve(echo, 'localhost', 8004)
-    asyncio.get_event_loop().run_until_complete(server)
-    asyncio.get_event_loop().run_forever()
-
+    loop = asyncio.get_event_loop()
+    server=asyncio.ensure_future(websockets.serve(handler, 'localhost', 8004))
+    try:
+        #loop.run_until_complete(server)
+        loop.run_forever()
+        loop.close()
+    except:
+        print("exception consumed")
