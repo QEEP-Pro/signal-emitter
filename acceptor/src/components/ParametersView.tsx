77389ed6c93e2f821534e99624850e7a87c5da9a
Igor Kamyshev <garik.novel@gmail.com>
2017-11-04 19:16:10 +0700

    [wip] acceptor

--- acceptor/src/components/ParametersView.tsx
+++ acceptor/src/components/ParametersView.tsx
@@ -6,11 +6,27 @@ interface Props {
 }
 
 export default class ParametersView extends React.Component<Props, {}> {
+
+    componentWillMount() {
+        this.componentWillReceiveProps(this.props)
+    }
+
+    componentWillReceiveProps(nextPops: Props) {
+        const socket = new WebSocket('ws://localhost:3000/sockjs-node/916/wkxbeewj/websocket')
+
+        socket.onopen = () => {
+            console.log('ok')
+        }
+    }
+
     render() {
-        console.log(this.props.ids)
+        const { ids } = this.props
 
         return(
-            <p>...</p>
+            <div>
+                <p>...</p>
+                {ids.map((id: number) => <p key={id}>{id}</p>)}
+            </div>
         )
     }
 }
