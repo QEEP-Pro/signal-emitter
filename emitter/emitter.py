e7f53e1457e59dc0a4f3fb3d2a0269c3e5a772b7
basilio <catred@mail2000.ru>
2017-11-04 19:15:39 +0700

    [emitter] websocket

--- emitter/emitter.py
+++ emitter/emitter.py
@@ -1,24 +1,21 @@
-import socketserver
+import asyncio
+import websockets
 from random import random
 
+async def echo(websocket, path):
+    async for message in websocket:
+        await websocket.send("[{0},{1}]".format(random(),random()))
 
-class MyTCPHandler(socketserver.StreamRequestHandler):
-    def handle(self):
-        data = self.rfile.readline().strip()
-        #print("{} wrote:".format(self.client_address[0]))
-        self.parse(data)
-        response = "[{0},{1}]".format(random(),random())
-        self.wfile.write(bytearray(response,'utf8'))
-
+'''
     def parse(self, data):
         if "refreshData" in str(data):
             pass
         elif "parametersList" in str(data):
             pass
-            
-        
+'''
         
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 80
-    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
-    server.serve_forever()
+    server=websockets.serve(echo, 'localhost', 8004)
+    asyncio.get_event_loop().run_until_complete(server)
+    asyncio.get_event_loop().run_forever()
+
