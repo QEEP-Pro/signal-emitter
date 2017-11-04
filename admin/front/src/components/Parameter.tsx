10c030bab059d8cbd3f13569258e51142590c94a
Igor Kamyshev <garik.novel@gmail.com>
2017-11-04 16:40:01 +0700

    fix layout

--- admin/front/src/components/Parameter.tsx
+++ admin/front/src/components/Parameter.tsx
@@ -15,14 +15,26 @@ interface Props {
 export default ({ parameter }: Props) => (
     <Card className={css`margin-top: 2rem;`}>
         <CardTitle title={parameter.name} />
-        <List>
-            <ListItem primaryText={getRangeWithUnit(parameter)} secondaryText={'Диапозон значений'} />
-            <ListItem primaryText={parameter.law.title} secondaryText={'Закон рапределения'} />
-            <ListItem primaryText={parameter.mean} secondaryText={'Математическое ожидание'} />
-            <ListItem primaryText={parameter.dispersion} secondaryText={'Дисперсия'} />
-        </List>
+        <div className={s.row}>
+            <List style={{width: '50%'}}>
+                <ListItem primaryText={getRangeWithUnit(parameter)} secondaryText={'Диапозон значений'} />
+                <ListItem primaryText={parameter.law.title} secondaryText={'Закон рапределения'} />
+            </List>
+            <List style={{width: '50%'}}>
+                <ListItem primaryText={parameter.mean} secondaryText={'Математическое ожидание'} />
+                <ListItem primaryText={parameter.dispersion} secondaryText={'Дисперсия'} />
+            </List>
+        </div>
     </Card>
 )
 
 const getRangeWithUnit = ({min, max, unit = false}: Parameter) =>
     `от ${min}${unit ? ` ${unit}` : null} до ${max}${unit ? ` ${unit}` : null}`
+
+const s = {
+    row: css`
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+    `,
+}
