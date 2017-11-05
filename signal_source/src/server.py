88a1b52424662db54b042451f1997bb07b34282d
Igor Kamyshev <garik.novel@gmail.com>
2017-11-05 13:31:16 +0700

    fix type bug in make_noise function

--- signal_source/src/server.py
+++ signal_source/src/server.py
@@ -11,7 +11,7 @@ from models import Parameter
 
 
 def make_noise(num, parameter):
-    noise_delta=math.sqrt(parameter.dispersion)/parameter.mean
+    noise_delta=math.sqrt(parameter.dispersion)/float(parameter.mean)
     return random.uniform(num*(1-noise_delta), num*(1+noise_delta))
 
 
