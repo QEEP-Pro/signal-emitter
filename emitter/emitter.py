7015195295fa7fde1e74e1404b118f199a47f5c5
BasilFaun <catred@mail2000.ru>
2017-11-05 09:36:38 +0700

    [emitter] pysql

--- emitter/emitter.py
+++ emitter/emitter.py
@@ -1,33 +1,50 @@
 import asyncio
 import websockets
 from random import random
+import pymysql.cursors
 
-'''
-    db: {
-        host     : 'localhost',
-        user     : 'admin',
-        password : 'admin',
-        database : 'digital-hack'
-    },
-    port: 3002,
-'''
 
 connection = dict()
+parameters = None
+conn_db = None
+
 _id=0
+
+async def consumer_handler(websocket, path):
+    while True:
+        message = await websocket.recv()
+        print(message)
+
+async def producer_handler(websocket, path):
+        message = "\"id\":1,\"name\":2,\"period\":3,\"min\":4,\"max\":5,\"mean\":6,\"unit\":7,\"noise\":8,\"dispersion\":9,\"law_id\":0"
+        await websocket.send(message)
+
 
 async def handler(websocket, path):
     global _id
-    _id+=1
+    global parameters
     global connection
+    global conn_db
+    _id+=1
     connection[websocket]=_id
     print(path)
     try:
         if path=="/api/refreshData":
-            pass
+            with conn_db.cursor() as cursor:
+                cursor.execute("SELECT * FROM `parameters`")
+                parameters = cursor.fetchall()
+                print(parameters)
         elif path=="/api/parametersList":
-            await websocket.send("[\"name\":,\"laws\":]")
-            lawsIdList=await websocket.recv()
-            print(lawsIdList)
+            consumer_task = asyncio.ensure_future(consumer_handler(websocket,path))
+            producer_task = asyncio.ensure_future(producer_handler(websocket,path))
+            done, pending = await asyncio.wait([consumer_task, producer_task],return_when=asyncio.FIRST_COMPLETED)
+
+            for task in pending:
+                task.cancel()
+            print("ololo")
+            #idParametersList = await websocket.recv()
+            #await websocket.send("[\"name\":,\"laws\":]")
+            #print(idParametersList)
         else:
             while len(connection)>0:
                 x,y=random(),random()
@@ -38,11 +55,22 @@ async def handler(websocket, path):
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    server=asyncio.ensure_future(websockets.serve(handler, 'localhost', 8004))
+    conn_db = pymysql.connect(host='localhost',user='root',password='pasS0511',db='digital-hack',charset='utf8',cursorclass=pymysql.cursors.DictCursor)
     try:
-        #loop.run_until_complete(server)
-        loop.run_forever()
-        loop.close()
-    except:
-        print("exception consumed")
+        with conn_db.cursor() as cursor:
+            cursor.execute("SELECT * FROM `parameters`")
+            parameters = cursor.fetchall()
+            print(parameters)
+
+        loop = asyncio.get_event_loop()
+        server=asyncio.ensure_future(websockets.serve(handler, 'localhost', 8004))
+        try:
+            #loop.run_until_complete(server)
+            loop.run_forever()
+            loop.close()
+        except:
+            print("exception consumed")
+    finally:
+        conn_db.close()
+
+
